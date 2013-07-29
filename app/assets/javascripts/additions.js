f84c2790faa3ca43b71358876e4c98737011d604
lucascampbell <lkecamp1@gmail.com>
2013-07-29 09:10:43 -0700

    new

--- app/assets/javascripts/additions.js
+++ app/assets/javascripts/additions.js
@@ -20,5 +20,33 @@ $(function() {
    return false;
  });
 
+  var count = 180;
+  var counter;
+
+  $("#timerbtn").click(function(){
+    clearInterval(counter);
+    count = 5;//$("#count").val();
+    counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+  })
+
+  function timer(){
+    count=count-1;
+    if (count <= 0)
+    {
+       clearInterval(counter);
+       //counter ended, do something here
+       var number = Math.floor((Math.random()*2)+1);
+       $("#timer")[0].innerHTML = "00";
+       //var snd = new Audio("Donkey"+number+".mp3"); // buffers automatically when created
+       var snd = new Audio("Donkey21.mp3")
+       snd.play();
+       return;
+    }
+
+    //Do code for showing the number of seconds here
+    $("#timer")[0].innerHTML = count;
+
+  }
+
 });
 
