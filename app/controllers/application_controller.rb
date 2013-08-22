9c5892c36c136e2cd4c38196ed814a3ef695b8a4
lucas campbell-rossen <lucas@rhomobile.com>
2013-08-22 14:45:42 -0700

    fix session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def get_user
-    league_id        = params[:league_id] || "testuser"
-    access_token     = params[:access_token] || "testtoken"
-    @user            = User.find_by_league_id(league_id)
+    session[:league_id]     = params[:league_id] || "testuser" unless session[:league_id]
+    session[:access_token]  = params[:access_token] || "testtoken" unless session[:access_token]
+    @user                   = User.find_by_league_id(session[:league_id])
     unless @user
-      @user = User.new(:email => "lucas.campbellrossen@gmail.com", :password => league_id, :password_confirmation => league_id,:league_id=>league_id,:access_token=>access_token)
+      @user = User.new(:email => "lucas.campbellrossen@gmail.com", :password => session[:league_id], :password_confirmation => session[:league_id],:league_id=>session[:league_id],:access_token=>session[:access_token])
       @user.save!
       sign_in @user, :bypass => true 
     else
-      @user.update_attribute("access_token",access_token)
+      @user.update_attribute("access_token",session[:access_token])
     end
     session[:draft_id] = Draft.find(params[:draft]).id if params[:draft]
     session[:draft_id] = Draft.create!({:name=>"#{Draft.count + 1}_#{Time.now.to_i}",:user_id=>@user.id}).id if session[:draft_id].nil?
