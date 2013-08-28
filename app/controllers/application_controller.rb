86fea85822251d63f5c3a4ec5f6bb2a698126362
lucas campbell-rossen <lucas@rhomobile.com>
2013-08-28 10:25:06 -0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,16 +18,16 @@ class ApplicationController < ActionController::Base
     unless user_signed_in?
       session[:league_id]     = params[:league_id] unless session[:league_id]
       session[:access_token]  = params[:access_token] unless session[:access_token]
-      @user                   = User.find_by_league_id(session[:league_id])
-      unless @user
-        @user = User.new(:email => "#{session[:league_id]}@gmail.com", :password => session[:league_id], :password_confirmation => session[:league_id],:league_id=>session[:league_id],:access_token=>session[:access_token])
-        @user.save!
-        sign_in @user, :bypass => true 
+      user                   = User.find_by_league_id(session[:league_id])
+      unless user
+        user = User.new(:email => "#{session[:league_id]}@gmail.com", :password => session[:league_id], :password_confirmation => session[:league_id],:league_id=>session[:league_id],:access_token=>session[:access_token])
+        user.save!
+        sign_in user, :bypass => true 
       else
-        @user.update_attribute("access_token",session[:access_token])
+        user.update_attribute("access_token",session[:access_token])
       end
     end
     session[:draft_id] = Draft.find(params[:draft]).id if params[:draft]
-    session[:draft_id] = Draft.create!({:name=>"#{Draft.count + 1}_#{Time.now.to_i}",:user_id=>current_user.id}).id if session[:draft_id].nil?
+    session[:draft_id] = Draft.create!({:name=>"#{Draft.count + 1}_#{Time.now.to_i}",:user_id=>user.id}).id if session[:draft_id].nil?
   end
 end
